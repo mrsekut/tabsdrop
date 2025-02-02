3b7e160f180e208582d1c1bbb7c7833fc3457543
mrsekut <k.cloudspider@gmail.com>
2025-02-02 13:58:02 +0900

    chore: update Prettier configuration for improved formatting rules

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,25 +2,8 @@
  * @type {import('prettier').Options}
  */
 export default {
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
-  semi: false,
-  singleQuote: false,
-  trailingComma: "none",
-  bracketSpacing: true,
-  bracketSameLine: true,
-  plugins: ["@ianvs/prettier-plugin-sort-imports"],
-  importOrder: [
-    "<BUILTIN_MODULES>", // Node.js built-in modules
-    "<THIRD_PARTY_MODULES>", // Imports not matched by other special words or groups.
-    "", // Empty line
-    "^@plasmo/(.*)$",
-    "",
-    "^@plasmohq/(.*)$",
-    "",
-    "^~(.*)$",
-    "",
-    "^[./]"
-  ]
+  arrowParens: "avoid",
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true
 }
