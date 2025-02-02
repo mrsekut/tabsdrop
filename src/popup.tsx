331b2848883a9f79ee32f0df1f4058dba3635ca1
mrsekut <k.cloudspider@gmail.com>
2025-02-02 17:06:39 +0900

    feat: add authentication check and user notification for Pocket configuration

--- src/popup.tsx
+++ src/popup.tsx
@@ -5,6 +5,26 @@ import { usePocketAccessToken } from './features/Pocket/usePocketAccessToken';
 import { usePocketConsumerKey } from './features/Pocket/usePocketCunsumerKey';
 
 export default function Popup() {
+  const { consumerKey } = usePocketConsumerKey();
+  const { accessToken } = usePocketAccessToken();
+
+  if (!consumerKey || !accessToken) {
+    return <NotAuthorized />;
+  }
+
+  return <PocketTabSaver />;
+}
+
+function NotAuthorized() {
+  return (
+    <div style={{ marginTop: '10px', fontSize: '12px', color: '#555' }}>
+      Pocket authentication information is not set. Please configure it on the
+      options page.
+    </div>
+  );
+}
+
+function PocketTabSaver() {
   const [targetTabs, setTargetTabs] = useState<TabInfo[]>([]);
 
   const { consumerKey } = usePocketConsumerKey();
