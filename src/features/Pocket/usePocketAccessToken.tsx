ff83ae7dd673456ca8d8a0f83f3b058d397ef4f0
mrsekut <k.cloudspider@gmail.com>
2025-06-14 23:48:24 +0900

    remove pocket

--- src/features/Pocket/usePocketAccessToken.tsx
+++
@@ -1,6 +0,0 @@
-import { useStorage } from "@plasmohq/storage/hook";
-
-export const usePocketAccessToken = () => {
-  const [accessToken, setAccessToken] = useStorage("accessToken", "");
-  return { accessToken, setAccessToken };
-};
--- src/features/Pocket/usePocketCunsumerKey.ts
+++
@@ -1,6 +0,0 @@
-import { useStorage } from "@plasmohq/storage/hook";
-
-export const usePocketConsumerKey = () => {
-  const [consumerKey, setConsumerKey] = useStorage("consumerKey", "")
-  return { consumerKey, setConsumerKey }
-}
